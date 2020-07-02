1b72945bb23ddc42e68a9d62ed00c31b2b532734
Harley Rogers <harleydev512@gmail.com>
2020-07-02 13:15:10 -0500

    Added classes and called them

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,34 +6,20 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
+            // TODO Be sure to follow best practice when creating your class
 
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
+            //name, color, song, preditor, int beekLength in inches, int legs, int age, home
+            Bird mockingBird = new Bird("Mocking Bird","black and White", "Mixed", true, 1, 2, 5, "Woods" );
+            Console.WriteLine($"Here are the features of {mockingBird.Name}, \n Color: {mockingBird.Color} \n What song does it make: {mockingBird.Song}" +
+                $"\n T or F, Is it a preditor: {mockingBird.Preditor} \n How long is its beak: {mockingBird.BeekLength} \n " +
+                $"Number of Legs: {mockingBird.Legs} \n Age: {mockingBird.Age} \n Home: {mockingBird.Home}");
+            Console.WriteLine();
+            //name, color, energy, bloodtype, food, int legs, int age, home 
+            Reptile beardedDragon = new Reptile("bearded Dragon", "tanish", "High", "Cold Blooded", "worms, crickets...", 4, 15, "My home!"  );
+            Console.WriteLine($"Here are the features of {beardedDragon.Name}, \n Color: {beardedDragon.Color} \n Energy Level: {beardedDragon.EnergyLevel}" +
+                $"\n Blood Type: {beardedDragon.BloodType} \n Food: {beardedDragon.Food} \n " +
+                $"Number of Legs: {beardedDragon.Legs} \n Age: {beardedDragon.Age} \n Home: {beardedDragon.Home}");
 
-
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-
-
-
-
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
-
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
         }
     }
 }
