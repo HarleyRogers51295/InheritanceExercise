0165f99a939948a95a15ce4f99b6ff5b59d4a598
Harley Rogers <harleydev512@gmail.com>
2020-07-02 13:16:50 -0500

    Added Classes

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Bird : Animal
+    {
+        public bool Preditor { get; set; }
+        public int BeekLength { get; set; }
+        public string Color { get; set; }
+        public string Song { get; set; }
+
+        public Bird( string name, string color, string song, bool preditor, int beekLength, int legs, int age, string home)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.Song = song;
+            this.Preditor = preditor;
+            this.BeekLength = beekLength;
+            this.Legs = legs;
+            this.Age = age;
+            this.Home = home;
+        }
+    }
+}
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Reptile : Animal
+    {
+        public string Color { get; set; }
+        public string EnergyLevel { get; set; }
+        public string BloodType { get; set; }
+        public string Food { get; set; }
+
+
+        public Reptile(string name , string color, string energy, string blood, string food, int legs, int age, string  home )
+        {
+            this.Name = name;
+            this.Color = color;
+            this.EnergyLevel = energy;
+            this.BloodType = blood;
+            this.Food = food;
+            this.Legs = legs;
+            this.Age = age;
+            this.Home = home;
+        }
+    }
+}
